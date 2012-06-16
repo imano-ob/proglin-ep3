befca7b0b6caf88312513948c144418f03165fe7
Renan Teruo <renanteruoc@gmail.com>
2012-06-16 19:55:21 -0300

    Parece que até a fase 1 ta funcionando

--- simplex.m
+++ simplex.m
@@ -7,14 +7,14 @@ function [ind x] = simplex(A,b,c,m,n,print)
 % Se não tivermos soluções satisfatórias, retornamos x vazio
   x = [];
 % Muda A e b para que b seja >= 0
-   for i = 1:m
+  for i = 1:m
      if b(i) < 0
-       A(i, 1:n) *= -1;
-       b(i) *= -1;
+       A(i, 1:n) = -A(i, 1:n);
+       b(i) = -b(i);
      endif 
    endfor
 % fase 1! Teremos, no retorno, um A sem restrições redundantes e um tableau preparado
-   tab = fase1(A,b,m,n,print);
+   tab = fase1(A,b,m,n,print)
 % Custo da fase 1 diferente de 0. Não é viável.
    if tab(1,1) != 0
      ind = 1;
@@ -27,22 +27,18 @@ endfunction
 
 
 function [A tab m] = fase1(A,b,m,n,print)
-  % Vetor de custos diferente para a fase 1. Somatória de y, onde cada y é uma variável artificial.
-  tmpc = [ zeros(n,1); ones(m,1)];
-  % Precisamos de um A maior para acomodar os y, e manter o problema da forma Ax = b. Assim, teremos [A 0] * [x y]' = Ax + y = b.
-  % tmpA = [ A zeros(m,m) ];
   % Geramos um tableau inicial
-  tab = gentab(A,b,tmpc,m,m+n);
+  tab = genPhase1Tab(A,b, m, n);
   % Iterações do simplex vão aqui.
-  [ind tab] = tabsimplex(m,m+n,print, tab);
+  [ind tab] = tabsimplex(m,m+n,print, tab)
   % Custo ótimo diferente de 0. Não é viável o problema.
   if tab(1,1) != 0
     return
   endif
   % Iteramos mais algumas vezes para remover variáveis artificais. Se alguma restar, arrumamos A.
-  [tab m] = fixA(tmpA, tab, m, m+n);
+  [tab m] = fixA(tab, m, m+n);
 endfunction
-
+,m
 
 
 function [ind x] = fase2(A,b,c,m,n,print,tab)
@@ -67,15 +63,15 @@ function [ind tab] = tabsimplex(m,n,print,tab)
   while sum(tab(1,2:n)<0) >= 1
     j = 2;
     %pega a primeira coluna com custo negativo
-    while tab( 1, j ) > 0
-      j++;
+    while tab( 1, j ) >= 0
+      ++j;
     endwhile
     minrow = 0;
     minval = Inf;
     % Escolhe o pivô; Se não houver nenhum candidato válido, custo é -Inf
     for i = 2:m
       if (tab(i, j) > 0)
-        t = tab(i, 1)/tab(i, j)
+        t = tab(i, 1)/tab(i, j);
         if t < minval
           minval = t;
           minrow = i;
@@ -102,13 +98,14 @@ endfunction
 
 
 
-function tab = gentab(A,b,c,m,n)
-  tab = zeros(m+1, n+1);
-  tab(1,1) = -(c(n-m+1:n)' * b);
+function tab = genPhase1Tab(A,b,m,n)
+  c = [zeros(n,1); ones(m,1)];
+  tab = zeros(m+1, m+n+1);
+  tab(1,1) = -(c(n+1:m+n)' * b);
   tab(2:m+1, 1) = b;
-  tab(2:m+1, 2:(n-m+1)) = A;
-  tab(2:m+1, n-m+2:n+1) = eye(m);
-  tab(1, 2:n+1) = c' - ones(1,m) * [A eye(m)]
+  tab(2:m+1, 2:n+1) = A;
+  tab(2:m+1, n+2:m+n+1) = eye(m);
+  tab(1, 2:n+m+1) = c' - ones(1,m) * [A eye(m)];
 endfunction 
 
 
