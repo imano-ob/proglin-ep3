abad64f10f35cdf8a9dbdb80bb41acd15b4fa34e
Renan Teruo <renanteruoc@gmail.com>
2012-06-16 14:18:45 -0300

    Mais progresso. Bagunçado ainda.

--- simplex.m
+++ simplex.m
@@ -3,41 +3,67 @@
 
 function [ind x] = simplex(A,b,c,m,n,print)
 % Does teh simplex ftw
+
+% Se não tivermos soluções satisfatórias, retornamos x vazio
+  x = [];
 % Muda A e b para que b seja >= 0
    [A b] = tornabpositivo(A,b,m);
-% fase 1!
-   [ind x A] = fase1(A,b,m,n,print);
-   if ind == 1
+% fase 1! Teremos, no retorno, um A sem restrições redundantes e um tableau preparado
+   [A tab] = fase1(A,b,m,n,print);
+% Custo da fase 1 diferente de 0. Não é viável.
+   if tab(1,1) != 0
+     ind = 1;
      return
    endif
-   [ind x] = fase2(A,b,c,m,n,print,x);
+% Fase 2 do simplex!
+   [ind x] = fase2(A,b,c,m,n,print,tab);
 endfunction
 
-function [ind x A] = fase1(A,b,m,n,print)
+function [ind A tab] = fase1(A,b,m,n,print)
   tmpc = [ zeros(n,1) ones(m,1)];
   tmpA = [ A zeros(m,m) ];
-  [ind x tab] = tabsimplex(tmpA,b,tmpc,m,m+n,print,[zeros(1,n) b], []);
-  if tab(1,1) > 0
-    x = 
-    ind = 1;
+  [ind tab] = tabsimplex(tmpA,b,tmpc,m,m+n,print,[zeros(1,n) b], []);
+  if tab(1,1) != 0
+    return
   endif
   [A tab x m] = fixA(tmpA, tab, x, m, n);
 endfunction
 
 function [ind x] = fase2(A,b,c,m,n,print,x)
-  [ind cost x tab] = tabsimplex(A,b,c,m,n,print,x,tab)
+  [ind tab] = tabsimplex(A,b,c,m,n,print,x,tab)
 endfunction
 
-function [ind cost x tab] = tabsimplex(A,b,c,m,n,print,x,tab)
+function [ind tab] = tabsimplex(A,b,c,m,n,print,x,tab)
   if tab == []
     tab = gentab(A,b,c,m,n,x);
   endif
-  while %algo
-    %rodaisimplex
-    if % custo vai ser menas infinito
+  % Enquanto tiver pelo menos um custo negativo na linha 0 do tableau
+  while sum(tab(1,2:n)<0) >= 1
+    j = 2;
+    %pega a primeira coluna com custo negativo
+    while tab( 1, j ) > 0
+      j++;
+    endwhile
+    minrow = 0;
+    minval = Inf;
+    % Escolhe o pivô; Se não houver nenhum candidato válido, custo é -Inf
+    for i = 2:m
+      if (tab(i, j) > 0)
+        t = tab(i, 1)/tab(i, j)
+        if t < minval
+          minval = t;
+          minrow = i;
+        endif
+      endif
+    endfor 
+    if minval == Inf 
       ind = 0;
       return
     endif
+    i = minrow;
+    % Divide a linha do pivô pelo pivô
+    tab(i, 1:n) = tab(i, 1:n) / tab(i,j);
+    
   endwhile
   ind = -1;
 endfunction
